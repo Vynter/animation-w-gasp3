304c09aba9de72b78f7ce19a4dd657eb3a8eb4f5
Vynter <amine-cheraitia@hotmail.com>
2020-09-21 16:47:39 +0200

    delay cmt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const timelane = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 timelane.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-timelane.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+timelane.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 }); // delay the animation of 0.5s
 timelane.to(".intro", { y: "-100%", duration: 1 }, "-=1.1");
 timelane.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 timelane.fromTo(
