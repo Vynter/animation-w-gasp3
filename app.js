f4832645690ae77ea1bc1a4ab0594965cc73dd3c
Vynter <amine-cheraitia@hotmail.com>
2020-09-21 14:29:36 +0200

    cmt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const timelane = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 timelane.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 timelane.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-timelane.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+timelane.to(".intro", { y: "-100%", duration: 1 }, "-=1.1");
 timelane.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 timelane.fromTo(
   ".mass-text",
@@ -11,3 +11,4 @@ timelane.fromTo(
   { opacity: 1, duration: 1 },
   "-=1"
 );
+//-=1 === start 1s before
